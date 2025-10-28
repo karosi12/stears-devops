ae75c8faae98682ddf8ed78af7ce53b1520f2d52
karosi12 <adekayor@gmail.com>
2025-10-28 20:23:28 +0100

    feat: update jenkins env

--- infra/generate_tfvars.sh
+++ infra/generate_tfvars.sh
@@ -12,4 +12,11 @@ rds_proxy_secret_name = "$RDS_PROXY_SECRET_NAME"
 access_key = "$AWS_ACCESS_KEY_ID"
 secret_key = "$AWS_SECRET_ACCESS_KEY"
 region = "$REGION"
+mq_username = "$MQ_USERNAME"
+mq_password = "$MQ_PASSWORD"
+host_instance_type = "$HOST_INSTANCE_TYPE"
+engine_version = "$ENGINE_VERSION"
+broker_name = "$BROKER_NAME"
+environment = "$ENVIRONMENT"
+broker_nodes = $BROKER_NODES
 EOF
